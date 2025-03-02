a7277c191b733ffa8ab280525f95536d7674c453
sarismejiasanchez <sarismejiasanchez@gmail.com>
2025-03-02 11:32:47 -0500

    Modulo 1. Introducción a Python

---
+++ 2_python_syntax.py
@@ -0,0 +1,56 @@
+print("Hello")
+print("World!")
+
+print("Hello"); print("World!")
+
+# any ammount of whitespace on a single line is ok
+x = 1 +             2
++  3
+print(x)
+
+x     =        1        +        2
+print(x)
+
+
+x = 1 +             2 \
++  3
+print(x)
+
+# Sangria
+
+name = "Jhon"
+if name == "Jhon":
+    print(name)
+
+def say_hello():
+    print("Hello there!")
+
+say_hello()
+
+def say_hello(): print("Hello there!")
+
+say_hello()
+
+# Comments
+# Comentario de una sola línea
+
+# Don't try to Run Me, I'm a comment
+x = 10
+
+# Comentario de varias líneas
+"""
+La siguiente función acepta dos enteros y
+suma ambos números para devolver el valor.
+"""
+def add_two_numbers(a, b):
+    return a + b
+
+'''
+La siguiente función acepta dos enteros y
+suma ambos números para devolver el valor.
+'''
+def add_two_numbers(a, b):
+    return a + b
+
+# Comentarios en línea
+x = 1 # Restablecer el tamaño del buffer
---
+++ 3_variables.py
@@ -0,0 +1,30 @@
+# CamelCase
+myName = "John"
+print(myName)
+
+# snake_case
+my_name = "Sara"
+print(my_name)
+
+x = 5
+print(x)
+
+a = b = c = 10
+print(a)
+print(b)
+print(c)
+
+a, b, c = 1, 2, 3
+print(a)
+print(b)
+print(c)
+
+a = 5
+print(a)
+
+a = 10
+print(a)
+
+del a
+
+# print(a)    # NameError: name 'a' is not defined
