2b04fb7cc78653fff04a608c9f9a3400a82dca8b
sarismejiasanchez <93800332+sarismejiasanchez@users.noreply.github.com>
2025-03-07 18:57:22 +0000

    Practicing control flows and loops

--- 12_loops.py
+++ 12_loops.py
@@ -34,10 +34,10 @@ for i in range(10): # Range function
     print("Looping...", i)
 
 # In this case, item calls each of the five dessert titles in turn and our print statement combines them into a sentence.
-favorites = ["Creme Brule", "Apple Pie", "Churros", "Tiramisu", "Chocolate Cake"]
+favorites = ["Creme Brulee", "Apple Pie", "Churros", "Tiramisú", "Chocolate Cake"]
 
-for desert in favorites:
-    print(f"I like this desert: {desert}")
+for dessert in favorites:
+    print(f"I like this dessert: {dessert}")
 
 # It's important to note that In a standard for loop, I don't have access to the index, 
 # but I can use the enumerate function to do that.
@@ -52,6 +52,44 @@ for idx, item in enumerate(favorites):
 count = 0
 
 while count < len(favorites):
-    print(f"I like this desert: {favorites[count]}")
+    print(f"I like this dessert: {favorites[count]}")
     count += 1
-    
+
+# PRACTICING CONTROL FLOWS AND LOOPS
+
+# if-else
+# Find the concrete element "Churros" in the list
+
+favorites = ["Creme Brulee", "Apple Pie", "Churros", "Tiramisú", "Chocolate Cake"]
+
+for dessert in favorites:
+    if dessert == "Churros":
+        print(f"Yes, One of my favorite desserts is {dessert}")
+
+# Find an element that is not in the list
+for dessert in favorites:
+    if dessert == "Pudding":
+        print(f"Yes, One of my favorite desserts is {dessert}")
+else: # This section will be executed if the loop is completed, even if it finds the element. 
+    print(f"No sorry, that dessert is not on my list")
+
+# Break statement
+for dessert in favorites:
+    if dessert == "Churros":
+        print(f"Yes, One of my favorite desserts is {dessert}")
+        break   # The break statement will stop the loop if the condition is met.
+else:
+    print(f"No sorry, that dessert is not on my list")
+
+# Continue statement
+# The continue statement will skip the current iteration and continue with the next iteration.
+for dessert in favorites:
+    if dessert == "Churros":
+        continue
+    print(f"Other desserts I like are {dessert}") # Print all the desserts except Churros
+
+# Pass statement
+for dessert in favorites:
+    if dessert == "Churros":
+        pass    # The pass statement will allow the loop to continue even if the condition is met.
+    print(f"Other desserts I like are {dessert}") # Print all the desserts including Churros
