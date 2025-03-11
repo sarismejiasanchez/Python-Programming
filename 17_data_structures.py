3e20a5728028adbbb4d71fb283d997edb30e6d40
sarismejiasanchez <93800332+sarismejiasanchez@users.noreply.github.com>
2025-03-11 16:20:10 +0000

    Data Structures

---
+++ 17_data_structures.py
@@ -0,0 +1,20 @@
+# DATA STRUCTURES
+"""
+Built-in Data Structures or non-primitive data structures
+- List
+- Tuple
+- Dictionary
+- Set
+
+User Defined Data Structures
+- Stack
+- Queue
+- Tree
+- Linked List
+- Graph
+- Hash Map
+
+Mutability or Immutability
+- Mutable: can be changed after creation. Example: List, Dictionary, Set, Byte Array
+- Inmutable: cannot be changed after creation. Example: Tuple, String, Numbers (int, float, complex), frozenset, bytes
+"""
