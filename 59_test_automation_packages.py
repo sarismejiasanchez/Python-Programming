b4bec859e686f9a9b540b1d007b84da50a00a36a
sarismejiasanchez <93800332+sarismejiasanchez@users.noreply.github.com>
2025-04-15 20:46:33 +0000

    Test Automation Packages

---
+++ 59_test_automation_packages.py
@@ -0,0 +1,60 @@
+# TEST AUTOMATION PACKAGES
+
+# In programming, the tests chosen for automation are the ones that have:
+# 1. High repeatability and volume
+# 2. Predictable environments and data
+# 3. Determinants outcomes.
+
+# There are a number of testing types that can be automated. 
+# These include units, regression, and integration.
+# An ideal test code must form a bridge between the programming codes and the test cases.
+
+# The ideal steps involved in test automation are usually:
+# 1. Preparing the test environment
+# 2. Running the test scripts
+# 3. Analyzing the results.
+
+# Let's now examine some important Python testing frameworks that have grown in popularity over the years.
+# First, let's explore the built-in testing package per unit or unittest.
+
+# The unittest framework supports test automation, independent testing modules, and aggregation of tests into collections. 
+
+# 1. Pytest and native Python library that is simple, easy to use, and reasonably scalable. 
+#    It can handle several functional test types such as unit, integration, and end-to-end. 
+#    There is support for parameterized testing which enables us to execute unit tests multiple times 
+#    with different parameters passed. 
+#    It can run parallel tests and generates HTML, XML, or plain texts reports. 
+#    You can also integrate it with other frameworks like PyUnit and Nose2, and web frameworks like Flask and Django.
+#    While primarily used with testing APIs, it's also well used with UI, database connections, and other web applications. 
+#    Easy creation and quick bug fixes a why Pytest is the most popular testing framework for automation. 
+# 
+# 2. Robot, which is popular primarily for its keyword-driven development capabilities. 
+#    These keywords are used in test cases and can be predefined or user-defined. 
+#    Robot is very versatile and use for:
+#    - Acceptance testing
+#    - Robotic Process Automation (RPA)
+#    - Test-Driven Development (TDD). 
+#    It can be used for many domains, including:
+#    - Android
+#    - APIs
+#    - Mainframes. 
+# 
+# 3. Selenium, is another open-source testing framework that has gained popularity over time, 
+#    and it's primarily driven towards a web applications. 
+#    It has support for the majority of web browsers and OS. 
+#    There a browser-specific web drivers that enable testing functionalities like 
+#    login, button clicks, and filling out forms.
+#    It allows the test set to select the speed and execution of tests and 
+#    it has an option to run specific tests or tests suites. 
+
+# Apart from the popular frameworks Pytest, Robot, and Selenium, there are many more. 
+# It's important to know that a number of these testing frameworks 
+# are often used with other tools such as plug-ins, widgets, extensions, test runners, and drivers. 
+# These tools help integrate the software pieces being tested and add functionality. 
+# Sometimes more than one framework has employed over the code being tested. 
+# 
+# Automation testing is an important reason why the software industry is able to move ahead swiftly and more smoothly. 
+# Manual testing provides focused attention and the ability to handle nuances and complex problems with more sophistication. 
+# This kind of testing can't be replaced by automated tests yet. 
+# It still sometime before test scenarios can be fully automated, 
+# but the development of all these frameworks are in line with that endeavor.
