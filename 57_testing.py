c8b1a106272213dddb8300dff613469e85ce4a14
sarismejiasanchez <93800332+sarismejiasanchez@users.noreply.github.com>
2025-04-15 19:24:25 +0000

    Testing

---
+++ 57_testing.py
@@ -0,0 +1,55 @@
+# WHAT IS TESTING?
+# Testing is the process of evaluating a system or its components to determine whether they satisfy specified requirements.
+
+# There are a few good practices that must be followed in testing to achieve optimal results. 
+# 1. Test code allowing reusability of tests. 
+# 2. Tests must be traceable to the requirements set. 
+# 3. Tests written must be purpose-driven, efficient, and allow for repeatability. 
+# These testing techniques can then follow a procedural approach according to the type of testing used. 
+
+# The testing lifecycle in general can be broadly described as:
+# 1. Planning
+# 2. Preparation
+# 3. Execution
+# 4. Reporting
+
+# The steps involved in achieving this can includE:
+# 1. Writing Scripts and test cases.
+# 2. Compiling test results.
+# 3. Correcting defects based on them.
+# 4. Generating reports from our test results.
+
+# They are a general sets of actions containing steps, data, pre and post-conditions written for a specific purpose.
+# This purpose can improve:
+# 1. Functionality
+# 2. Flow
+# 3. Finding defects. 
+
+# A well-written test case eventually provides:
+# 1. Good coverage
+# 2. Reusability
+# 3. Better user experience
+# 4. Reduces costs
+# 5. Increases overall satisfaction.
+
+# There are also many testing types using practice. These include:
+# 1. Compatibility
+# 2. Ad hoc
+# 3. Usability
+# 4. Regression testing. 
+
+# For now, I just want you to know that with testing, there is no one-size-fits-all solution. 
+#  When testing products, it's also important to understand when to stop. 
+# There is no application will ever be 100 percent perfect. 
+# Otherwise, a developer may feel the product is well-tested, but realize it's full of bugs 
+# and flaws as soon as it's released to the end-users. 
+
+# A few metrics can be established for this purpose, given that there are well-written test cases in place. These include:
+# 1. A certain number of tests cycles
+# 2. Passing percentage of test cases
+# 3. Time deadlines
+# 4. Time intervals between subsequent test failures. 
+
+# Testing in software development can be seen as the anchor of a ship or insurance for your vehicle. 
+# You can hope that everything operates smoothly, but often it does not and while you can aim for perfection, 
+# there is always potential for human error.
