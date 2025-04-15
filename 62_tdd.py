238c02fd4280428b6f4e0514ada78b431844b930
sarismejiasanchez <93800332+sarismejiasanchez@users.noreply.github.com>
2025-04-15 21:58:35 +0000

    Test Driven Development (TDD)

---
+++ 62_tdd.py
@@ -0,0 +1,47 @@
+# TEST DRIVEN DEVELOPMENT (TDD)
+
+# Test-driven development, or TDD, is an alternative programming practice in which 
+# the tests are written first and the code is written so that the tests will pass.
+
+
+# Test Driven Development (TDD) is a software development process that relies on the repetition of a very short development cycle:
+# 1. Write a test for a feature that fails.
+# 2. Write code in accordance with the tests.
+# 3. Run the tests expecting them to fail.
+# 4. Evaluate the error and refactor the code as needed.
+# 5. Rerun the process.
+
+# This process is also called the red green refactor cycle. 
+# - Red implies the failed tests
+# - Green shows the passing tests after refactoring. 
+# 
+# The whole point of following this cycle is to fail the tests and rewrite until you don't have to. 
+# A feature is complete when everything is green and you no longer need to rerun. 
+# 
+# You can use a package libraries such as PyTest when automation becomes a priority. 
+# Pytest only requires writing functions while a unit test requires classes. 
+# This means that pytest has the advantage of being easier because it requires less effort. 
+# 
+# Advantages of TDD
+# 1. Writing tests first and refactoring code based on it, ensures the test cover the code. 
+#    You can now write tests with a specific feature and outcome in mind. 
+# 2. The need for such forecasting provides clarity from the beginning. 
+# 3. The forecasting also plays a role in integrating different components 
+#    where you add new features and interface is in accordance with the components that are already there. 
+# 4. Working in cycles over the code, it gives us develop a competence that easily refactor in terms of additional changes.
+
+# Overall, smaller code with early bug fixes, code extensibility and eventual ease of debugging 
+# of the primary reasons TDD is growing and acceptance. 
+
+# Differences between TDD and traditional testing. 
+# The main difference is that with TDD, the requirements and standards 
+# are highlighted from the beginning, making it purposeful. 
+# 
+# Modern-day development often employs a combination of both these forms of testing, 
+# depending on the different parts and stages of the software development cycle. 
+
+# There are several subtypes and variations of test-driven development. These include: 
+# 1. Behavior-driven
+# 2. Acceptance test-driven
+# 3. Scaling and develop a test-driven development. 
+# These are all options that can be used in the software development process. 
